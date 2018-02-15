ad436c68d3bb90fe0e038d77b46daacbb3e749da
hero-007 <akhil1998tiwari@gmail.com>
2018-02-15 20:18:10 +0530

    add new_user() method to user_authorization.py

--- user_authorization.py
+++ user_authorization.py
@@ -12,4 +12,31 @@ def validate_user():
         return 1
 
 def new_user():
+    data_list = []
+    jarvis_dir = input("Enter the path to J.R.V.I.S directory : ")
+    jarvis_dir = jarvis_dir.replace('\\','\\\\')
+    data_list.append(jarvis_dir)
+    username = input("Enter Your Username : ")
+    data_list.append(username)
+    MS_Word = input("Enter the path of MS Word .exe file : ")
+    MS_Word = MS_Word.replace('\\','\\\\')
+    data_list.append(MS_Word)
+    MS_Excel = input("Enter the path of MS Excel .exe file : ")
+    MS_Excel = MS_Excel.replace('\\','\\\\')
+    data_list.append(MS_Excel)
+    MS_Power = input("Enter the path of MS Power Point .exe file : ")
+    MS_Power = MS_Power.replace('\\','\\\\')
+    data_list.append(MS_Power)
+    MS_Access = input("Enter the path of MS Access .exe file : ")
+    MS_Access = MS_Access.replace('\\','\\\\')
+    data_list.append(MS_Access)
+    MS_Outlook = input("Enter the path of MS Outlook .exe file : ")
+    MS_Outlook = MS_Outlook.replace('\\','\\\\')
+    data_list.append(MS_Outlook)
+
+    f = open('user_information.txt','w')
+    for i in data_list:
+        print(i,file=f)
+    f.close()
+    return
     
